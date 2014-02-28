021d6a7637b0c91b81c51798d498ed4e36f2ecd6
Trung Nguyen <trung.ngvan@gmail.com>
2014-02-28 17:01:39 +1100

    misc

--- standardize.m
+++ standardize.m
@@ -1,17 +1,16 @@
-function [X, Xmean, Xstd] = standardize(X, dim, Xmean, Xstd)
-%STANDARDIZE X = standardize(X, dim, X_mean, X_std)
+function [X, Xmean, Xstd] = standardize(X, Xmean, Xstd)
+%STANDARDIZE X = standardize(X, X_mean, X_std)
 %
 % Standardize a matrix to be normally distributed along a dimension using
 % the provided mean and std. X is usually the feature matrix of size NxD
 % where N is the number of observations and D is the input dimension.
 % 
-% This function can handle to missing data case:
+% This function can handle the missing data case:
 % all missing data should be given the value NaN which will be ignored when
 % performing normalization.
 %
 % INPUT
 %   - X : the matrix (usually feature matrix)
-%   - dim : the dimension along which the matrix is standardized
 %   - Xmean : mean of the training data (when applicable)
 %   - Xstd : std of the training data (when applicable)
 %
